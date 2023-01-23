f4c5feb2703ad0e8ed9b438fa7cef1b14fc13134
unknown <talakhatib4@gmail.com>
2023-01-23 12:57:41 +0200

    invoice sheet

--- spec/mailers/previews/client_billing_preview.rb
+++ spec/mailers/previews/client_billing_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/client_billing
 class ClientBillingPreview < ActionMailer::Preview
 
+    def billing 
+        client = Client.first
+        ClientBillingMailer.billing_mail(client)
+    end
+
 end
