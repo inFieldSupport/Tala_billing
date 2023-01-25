9e09ea50d24e4ff546ca22aad709fed40e3749bc
unknown <talakhatib4@gmail.com>
2023-01-25 14:24:59 +0200

    updating

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,5 +5,5 @@ class Employee < ApplicationRecord
     validates :password, presence: true
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum:255}, format: { with: VALID_EMAIL_REGEX } , uniqueness: true
+    validates :email, presence: true, length: { maximum:255}, format: { with: VALID_EMAIL_REGEX } 
 end
