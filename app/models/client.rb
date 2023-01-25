4415cf06fa5acf5ff93cfe067ca5a9bee5b9240d
unknown <talakhatib4@gmail.com>
2023-01-25 14:49:27 +0200

    update

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,5 @@ class Client < ApplicationRecord
     validates :client_name, presence: true
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum:255}, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, length: { maximum:255}, format: { with: VALID_EMAIL_REGEX } , uniqueness: true
 end
